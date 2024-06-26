d7c3bcb479dbc0ddf6e82d76ccc4291de81c4c6d
Fatoumata <85653301+Fatoumata-art@users.noreply.github.com>
2024-06-26 16:12:03 +0200

    working on Score chart

--- src/components/charts/chart.jsx
+++ src/components/charts/chart.jsx
@@ -14,13 +14,19 @@ function Chart(props){
           <BarChart
             width={500}
             height={300}
+            margin={{
+              top: 40,
+              right: 30,
+              left: 30,
+              bottom: 0,
+            }}
             data={props.data}
             key={Chart}
            
           >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey={props.name} />
-            <Legend legendType="circle" align="right"verticalAlign="top"/>
+            <Legend legendType="circle" align="right"verticalAlign="top" wrapperStyle={{top: 20}}/>
             <Tooltip />
             <Bar yAxisId="left" dataKey="uv" fill="#282D30" barSize={15} radius={8}/>
             <Bar yAxisId="right" dataKey="pv" fill="#E60000" barSize={15} radius={8}/>
